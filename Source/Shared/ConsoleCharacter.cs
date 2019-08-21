b6df10d028327ad690f2fbe7c5e86c8b0ead78b3
LtLi0n <31516441+LtLi0n@users.noreply.github.com>
2019-08-22 02:06:19 +0300

    Refactored Color object.
    Moved ColorPalette inside Color object.
    
    Changed ConsoleCharacter from enum, to loose ushort. Can still reference const values the same way as enum.

---
+++ Source/Shared/ConsoleCharacter.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleGameEngine
+{
+    public static class ConsoleCharacter
+    {
+        public const ushort NULL = 0x0000;
+        public const ushort FULL = 0x2588;
+        public const ushort Dark = 0x2593;
+        public const ushort Medium = 0x2592;
+        public const ushort Light = 0x2591;
+
+        // box drawing syboler
+        // ┌───────┐
+        // │       │
+        // │       │
+        // └───────┘
+        public const ushort BoxDrawingL_H = 0x2500;
+        public const ushort BoxDrawingL_V = 0x2502;
+        public const ushort BoxDrawingL_DR = 0x250C;
+        public const ushort BoxDrawingL_DL = 0x2510;
+        public const ushort BoxDrawingL_UL = 0x2518;
+        public const ushort BoxDrawingL_UR = 0x2514;
+    }
+}
