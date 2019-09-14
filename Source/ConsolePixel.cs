d49d02b967cb6430a4b6eaad9dae486980e3ece0
LtLi0n <LtLi0n@users.noreply.github.com>
2019-09-14 23:55:48 +0300

    Added ConsolePixel to display colored glyphs.

---
+++ Source/ConsolePixel.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleGameEngine
+{
+    public struct ConsolePixel
+    {
+        public byte Color { get; }
+        public char Glyph { get; }
+
+        public ConsolePixel(byte col, char glyph)
+        {
+            Color = col;
+            Glyph = glyph;
+        }
+    }
+}
